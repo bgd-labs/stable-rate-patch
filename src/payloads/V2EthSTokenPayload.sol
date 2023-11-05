707934f41d91701eb651ee8f2808c8c92609b96e
sendra <polsendra@gmail.com>
2023-11-05 20:39:34 +0100

    fix: added experimental

--- src/payloads/V2EthSTokenPayload.sol
+++ src/payloads/V2EthSTokenPayload.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity >=0.6.12;
+pragma experimental ABIEncoderV2;
 
 import {AaveV2Ethereum, AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
 import {MiscEthereum} from "aave-address-book/MiscEthereum.sol";
@@ -12,6 +13,18 @@ contract V2EthTokenPayload {
   }
 
   function execute() external {
+
+    TokenToUpdate[] memory tokensToUpdate = getTokensToUpdate();
+
+    for (uint256 i = 0; i < tokensToUpdate.length; i++) {
+      AaveV2Ethereum.POOL_CONFIGURATOR.updateStableDebtToken(
+        tokensToUpdate[i].underlyingAsset,
+        tokensToUpdate[i].newSTokenImpl
+      );
+    }
+  }
+
+  function getTokensToUpdate() public returns (TokenToUpdate[] memory){
     TokenToUpdate[] memory tokensToUpdate = new TokenToUpdate[](13);
 
     tokensToUpdate[0] = TokenToUpdate({
@@ -67,11 +80,6 @@ contract V2EthTokenPayload {
       newSTokenImpl: address(0) // TODO: fill with deployed address
     });
 
-    for (uint256 i = 0; i < tokensToUpdate.length; i++) {
-      AaveV2Ethereum.POOL_CONFIGURATOR.updateStableDebtToken(
-        tokensToUpdate[i].underlyingAsset,
-        tokensToUpdate[i].newSTokenImpl
-      );
-    }
+    return tokensToUpdate;
   }
 }
