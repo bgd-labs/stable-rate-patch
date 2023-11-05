148658aa0799af3d332f0ebf3c9cf15c3ab93545
sendra <polsendra@gmail.com>
2023-11-05 19:51:08 +0100

    fix: added prettier

--- scripts/DeploySTokenV3Avax.s.sol
+++ scripts/DeploySTokenV3Avax.s.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.8.0;
 
-import "forge-std/Script.sol";
+import 'forge-std/Script.sol';
 import {StableDebtToken} from '../src/v3AvaStableDebtToken/StableDebtToken/lib/aave-v3-core/contracts/protocol/tokenization/StableDebtToken.sol';
 import {AaveV3Avalanche} from 'aave-address-book/AaveV3Avalanche.sol';
 import {IPool} from '../src/v3AvaStableDebtToken/StableDebtToken/lib/aave-v3-core/contracts/interfaces/IPool.sol';
 import {IERC20Detailed} from '../src/v3AvaStableDebtToken/StableDebtToken/lib/aave-v3-core/contracts/dependencies/openzeppelin/contracts/IERC20Detailed.sol';
-
 
 contract BaseDeploy {
   function _deploy() internal {
     address[] memory reserves = AaveV3Avalanche.POOL.getReservesList();
 
     for (uint256 i = 0; i < reserves.length; i++) {
-      (,,,,,,, bool stableBorrowRateEnabled,,) =
-                  AaveV3Avalanche.AAVE_PROTOCOL_DATA_PROVIDER.getReserveConfigurationData(reserves[i]);
+      (, , , , , , , bool stableBorrowRateEnabled, , ) = AaveV3Avalanche
+        .AAVE_PROTOCOL_DATA_PROVIDER
+        .getReserveConfigurationData(reserves[i]);
       (, address stableDebtTokenAddress, ) = AaveV3Avalanche
         .AAVE_PROTOCOL_DATA_PROVIDER
         .getReserveTokensAddresses(reserves[i]);
@@ -23,9 +23,7 @@ contract BaseDeploy {
         continue;
       }
 
-      StableDebtToken newStableDebtImpl = new StableDebtToken(
-        IPool(address(AaveV3Avalanche.POOL))
-      );
+      StableDebtToken newStableDebtImpl = new StableDebtToken(IPool(address(AaveV3Avalanche.POOL)));
 
       console.log(
         IERC20Detailed(stableDebtTokenAddress).symbol(),
--- scripts/DeploySTokenV3Pol.s.sol
+++ scripts/DeploySTokenV3Pol.s.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.8.0;
 
-import "forge-std/Script.sol";
+import 'forge-std/Script.sol';
 import {StableDebtToken} from '../src/v3PolStableDebtToken/StableDebtToken/lib/aave-v3-core/contracts/protocol/tokenization/StableDebtToken.sol';
 import {AaveV3Polygon} from 'aave-address-book/AaveV3Polygon.sol';
 import {IPool} from '../src/v3PolStableDebtToken/StableDebtToken/lib/aave-v3-core/contracts/interfaces/IPool.sol';
 import {IERC20Detailed} from '../src/v3PolStableDebtToken/StableDebtToken/lib/aave-v3-core/contracts/dependencies/openzeppelin/contracts/IERC20Detailed.sol';
-
 
 contract BaseDeploy {
   function _deploy() internal {
     address[] memory reserves = AaveV3Polygon.POOL.getReservesList();
 
     for (uint256 i = 0; i < reserves.length; i++) {
-      (,,,,,,, bool stableBorrowRateEnabled,,) =
-                  AaveV3Polygon.AAVE_PROTOCOL_DATA_PROVIDER.getReserveConfigurationData(reserves[i]);
+      (, , , , , , , bool stableBorrowRateEnabled, , ) = AaveV3Polygon
+        .AAVE_PROTOCOL_DATA_PROVIDER
+        .getReserveConfigurationData(reserves[i]);
       (, address stableDebtTokenAddress, ) = AaveV3Polygon
         .AAVE_PROTOCOL_DATA_PROVIDER
         .getReserveTokensAddresses(reserves[i]);
@@ -23,9 +23,7 @@ contract BaseDeploy {
         continue;
       }
 
-      StableDebtToken newStableDebtImpl = new StableDebtToken(
-        IPool(address(AaveV3Polygon.POOL))
-      );
+      StableDebtToken newStableDebtImpl = new StableDebtToken(IPool(address(AaveV3Polygon.POOL)));
 
       console.log(
         IERC20Detailed(stableDebtTokenAddress).symbol(),
