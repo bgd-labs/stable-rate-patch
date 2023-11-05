90963685ed2ffcd6f0c2ad52f2e4b5e42d851e66
Andrei Kozlov <kyzia.ru@gmail.com>
2023-11-05 20:31:26 +0200

    add console.log and formatting into v2 eth script

--- scripts/DeploySTokenV2Eth.s.sol
+++ scripts/DeploySTokenV2Eth.s.sol
@@ -2,33 +2,40 @@
 pragma solidity >=0.6.12;
 pragma experimental ABIEncoderV2;
 
-import "forge-std/Script.sol";
+import 'forge-std/Script.sol';
 import {StableDebtToken} from '../src/v2EthStableDebtToken/StableDebtToken/contracts/protocol/tokenization/StableDebtToken.sol';
 import {AaveV2Ethereum} from 'aave-address-book/AaveV2Ethereum.sol';
 import {DataTypes} from '../src/v2EthStableDebtToken/StableDebtToken/contracts/protocol/libraries/types/DataTypes.sol';
 import {IERC20Detailed} from '../src/v2EthStableDebtToken/StableDebtToken/contracts/dependencies/openzeppelin/contracts/IERC20Detailed.sol';
-
 
 contract BaseDeploy {
   function _deploy() internal {
     address[] memory reserves = AaveV2Ethereum.POOL.getReservesList();
 
     for (uint256 i = 0; i < reserves.length; i++) {
-      (,,,,,,, bool stableBorrowRateEnabled,,) =
-                  AaveV2Ethereum.AAVE_PROTOCOL_DATA_PROVIDER.getReserveConfigurationData(reserves[i]);
-      (,address stableDebtTokenAddress,) =
-                  AaveV2Ethereum.AAVE_PROTOCOL_DATA_PROVIDER.getReserveTokensAddresses(reserves[i]);
+      (, , , , , , , bool stableBorrowRateEnabled, , ) = AaveV2Ethereum
+        .AAVE_PROTOCOL_DATA_PROVIDER
+        .getReserveConfigurationData(reserves[i]);
+      (, address stableDebtTokenAddress, ) = AaveV2Ethereum
+        .AAVE_PROTOCOL_DATA_PROVIDER
+        .getReserveTokensAddresses(reserves[i]);
 
       if (!stableBorrowRateEnabled) {
         continue;
       }
 
-      new StableDebtToken(
+      StableDebtToken newStableDebtImpl = new StableDebtToken(
         address(AaveV2Ethereum.POOL),
         reserves[i],
         IERC20Detailed(stableDebtTokenAddress).name(),
         IERC20Detailed(stableDebtTokenAddress).symbol(),
         address(0) // we leave as address 0 because all the static tokens where deployed with 0 for incentives controller
+      );
+
+      console.log(
+        IERC20Detailed(stableDebtTokenAddress).symbol(),
+        stableDebtTokenAddress,
+        address(newStableDebtImpl)
       );
     }
   }
