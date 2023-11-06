9344ebd29ff8159721f431d656cd656d89eb6f1a
sakulstra <lukasstrassel@googlemail.com>
2023-11-06 17:16:31 +0100

    fix: cleaner tests

--- tests/V2EthSToken.t.sol
+++ tests/V2EthSToken.t.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity >=0.6.12;
+pragma experimental ABIEncoderV2;
 
 import 'forge-std/Test.sol';
 import {BaseDeploy, StableToken} from '../scripts/DeploySTokenV2Eth.s.sol';
