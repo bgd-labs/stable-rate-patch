3efcb7b8525c54d6cc29d0c95e2648319d303190
Harsh Pandey <harshsatishpandey@gmail.com>
2023-11-06 23:34:23 +0530

    chore: split js scripts

--- js_scripts/diffStableToken.ts
+++ js_scripts/diffStableToken.ts
@@ -3,7 +3,7 @@
 import child_process from 'child_process';
 import dotenv from 'dotenv';
 import fs from 'fs';
-import {AaveV2Ethereum, AaveV3Polygon, AaveV3Avalanche, AaveV3Optimism, AaveV3Arbitrum} from "@bgd-labs/aave-address-book";
+import {AaveV2Ethereum} from "@bgd-labs/aave-address-book";
 dotenv.config();
 
 function runCmd(cmd: string) {
@@ -116,60 +116,8 @@ async function diffV2EthStableImpl() {
   }
 }
 
-async function getV3StableDebtImpl() {
-  const v3PolStableTokens = [];
-  const v3AvaStableTokens = [];
-  const v3OptStableTokens = [];
-  const v3ArbStableTokens = [];
-
-  for (const [key, value] of Object.entries(AaveV3Polygon.ASSETS)) {
-    // @ts-ignore
-    v3PolStableTokens.push(value.S_TOKEN);
-  }
-  for (const [key, value] of Object.entries(AaveV3Avalanche.ASSETS)) {
-    // @ts-ignore
-    v3AvaStableTokens.push(value.S_TOKEN);
-  }
-  for (const [key, value] of Object.entries(AaveV3Optimism.ASSETS)) {
-    // @ts-ignore
-    v3OptStableTokens.push(value.S_TOKEN);
-  }
-  for (const [key, value] of Object.entries(AaveV3Arbitrum.ASSETS)) {
-    // @ts-ignore
-    v3ArbStableTokens.push(value.S_TOKEN);
-  }
-
-  // we see that all the stable debt impl for aave v3 are same in one network
-
-  console.log('Polygon stable debt impl addresses:');
-  for (let i = 0; i < v3PolStableTokens.length; i++) {
-    const addr = getImpl('polygon', v3PolStableTokens[i]);
-    console.log('impl: ', addr, ' proxy:', v3PolStableTokens[i]);
-  }
-
-  console.log('Avalanche stable debt impl addresses:');
-  for (let i = 0; i < v3AvaStableTokens.length; i++) {
-    const addr = getImpl('avalanche', v3AvaStableTokens[i]);
-    console.log('impl: ', addr, ' proxy:', v3AvaStableTokens[i]);
-  }
-
-  console.log('Optimism stable debt impl addresses:');
-  for (let i = 0; i < v3OptStableTokens.length; i++) {
-    const addr = getImpl('optimism', v3OptStableTokens[i]);
-    console.log('impl: ', addr, ' proxy:', v3OptStableTokens[i]);
-  }
-
-  console.log('Arbitrum stable debt impl addresses:');
-  for (let i = 0; i < v3ArbStableTokens.length; i++) {
-    const addr = getImpl('arbitrum', v3ArbStableTokens[i]);
-    console.log('impl: ', addr, ' proxy:', v3ArbStableTokens[i]);
-  }
-
-}
-
 async function main() {
   diffV2EthStableImpl();
-  getV3StableDebtImpl();
 }
 
 main();
