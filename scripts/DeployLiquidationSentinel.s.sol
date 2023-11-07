183b39acd4cf39df409e3107846e10e179e71430
sakulstra <lukasstrassel@googlemail.com>
2023-11-07 14:59:01 +0100

    feat: add ava & polygon sentinel

--- scripts/DeployLiquidationSentinel.s.sol
+++ scripts/DeployLiquidationSentinel.s.sol
@@ -3,13 +3,17 @@ pragma solidity >=0.6.12;
 
 import 'forge-std/Script.sol';
 import {AaveV2Ethereum} from 'aave-address-book/AaveV2Ethereum.sol';
+import {AaveV2Polygon} from 'aave-address-book/AaveV2Polygon.sol';
+import {AaveV2Avalanche} from 'aave-address-book/AaveV2Avalanche.sol';
 
 import {LendingPoolCollateralManager} from '../src/v2EthLendingPoolCollateralManager/LendingPoolCollateralManager/contracts/protocol/lendingpool/LendingPoolCollateralManager.sol';
 import {LiquidationsGraceSentinel} from '../src/LiquidationsGraceSentinel.sol';
 import {V2EthLiquidationSentinelPayload} from '../src/payloads/V2EthLiquidationSentinelPayload.sol';
+import {V2PolLiquidationSentinelPayload} from '../src/payloads/V2PolLiquidationSentinelPayload.sol';
+import {V2AvaLiquidationSentinelPayload} from '../src/payloads/V2AvaLiquidationSentinelPayload.sol';
 
 library LiquidationSentinelDeployer {
-  function deployProposal() internal returns (address) {
+  function deployProposalEth() internal returns (address) {
     LiquidationsGraceSentinel sentinel = new LiquidationsGraceSentinel();
     sentinel.transferOwnership(AaveV2Ethereum.EMERGENCY_ADMIN);
     LendingPoolCollateralManager newCollateralManager = new LendingPoolCollateralManager(
@@ -17,12 +21,46 @@ library LiquidationSentinelDeployer {
     );
     return address(new V2EthLiquidationSentinelPayload(address(newCollateralManager)));
   }
+
+  function deployProposalAva() internal returns (address) {
+    LiquidationsGraceSentinel sentinel = new LiquidationsGraceSentinel();
+    sentinel.transferOwnership(AaveV2Avalanche.EMERGENCY_ADMIN);
+    LendingPoolCollateralManager newCollateralManager = new LendingPoolCollateralManager(
+      address(sentinel)
+    );
+    return address(new V2EthLiquidationSentinelPayload(address(newCollateralManager)));
+  }
+
+  function deployProposalPol() internal returns (address) {
+    LiquidationsGraceSentinel sentinel = new LiquidationsGraceSentinel();
+    sentinel.transferOwnership(AaveV2Polygon.EMERGENCY_ADMIN);
+    LendingPoolCollateralManager newCollateralManager = new LendingPoolCollateralManager(
+      address(sentinel)
+    );
+    return address(new V2EthLiquidationSentinelPayload(address(newCollateralManager)));
+  }
 }
 
-contract DeployLiquidationSentinel is Script {
+contract DeployLiquidationSentinelEth is Script {
   function run() public {
     vm.startBroadcast();
-    LiquidationSentinelDeployer.deployProposal();
+    LiquidationSentinelDeployer.deployProposalEth();
+    vm.stopBroadcast();
+  }
+}
+
+contract DeployLiquidationSentinelPol is Script {
+  function run() public {
+    vm.startBroadcast();
+    LiquidationSentinelDeployer.deployProposalPol();
+    vm.stopBroadcast();
+  }
+}
+
+contract DeployLiquidationSentinelAva is Script {
+  function run() public {
+    vm.startBroadcast();
+    LiquidationSentinelDeployer.deployProposalAva();
     vm.stopBroadcast();
   }
 }
